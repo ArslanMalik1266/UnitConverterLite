3e5ffe4c175f73f25c3f2fa9d1becb6163ac7986
Arslan Malik <mailk.arslan.ap@gmail.com>
2026-02-08 19:11:05 +0500

    updated

--- app/src/main/java/com/example/unitconverterlite/Adaptor/HistoryAdaptor.kt
+++ app/src/main/java/com/example/unitconverterlite/Adaptor/HistoryAdaptor.kt
@@ -45,7 +45,8 @@ class HistoryAdaptor(
 
     fun View.getUnitString(key: String): String {
         return when (key.lowercase()) {
-            // Categories / Unit types
+
+            // Categories
             "length" -> context.getString(R.string.length)
             "weight" -> context.getString(R.string.weight)
             "area" -> context.getString(R.string.area)
@@ -65,107 +66,106 @@ class HistoryAdaptor(
             "sound" -> context.getString(R.string.sound)
             "bmi" -> context.getString(R.string.bmi)
 
-            // Length units
-            "meter" -> context.getString(R.string.meter)
-            "kilometer" -> context.getString(R.string.kilometer)
-            "mile" -> context.getString(R.string.mile)
-            "foot" -> context.getString(R.string.foot)
-            "feet" -> context.getString(R.string.feet)
-            "inch" -> context.getString(R.string.inch)
+            // Length
+            "meter (m)" -> context.getString(R.string.meter)
+            "kilometer (km)" -> context.getString(R.string.kilometer)
+            "mile (mi)" -> context.getString(R.string.mile)
+            "foot (ft)" -> context.getString(R.string.foot)
+            "inch (in)" -> context.getString(R.string.inch)
 
-            // Area units
-            "square_meter" -> context.getString(R.string.square_meter)
-            "square_kilometer" -> context.getString(R.string.square_kilometer)
-            "square_mile" -> context.getString(R.string.square_mile)
-            "hectare" -> context.getString(R.string.hectare)
-            "acre" -> context.getString(R.string.acre)
+            // Area
+            "square meter (m²)" -> context.getString(R.string.square_meter)
+            "square kilometer (km²)" -> context.getString(R.string.square_kilometer)
+            "square mile (mi²)" -> context.getString(R.string.square_mile)
+            "hectare (ha)" -> context.getString(R.string.hectare)
+            "acre (ac)" -> context.getString(R.string.acre)
 
-            // Volume units
-            "liter" -> context.getString(R.string.liter)
-            "milliliter" -> context.getString(R.string.milliliter)
-            "gallon" -> context.getString(R.string.gallon)
-            "cubic_meter" -> context.getString(R.string.cubic_meter)
-            "cubic_foot" -> context.getString(R.string.cubic_foot)
+            // Volume
+            "liter (l)" -> context.getString(R.string.liter)
+            "milliliter (ml)" -> context.getString(R.string.milliliter)
+            "gallon (gal)" -> context.getString(R.string.gallon)
+            "cubic meter (m³)" -> context.getString(R.string.cubic_meter)
+            "cubic foot (ft³)" -> context.getString(R.string.cubic_foot)
 
-            // Weight units
-            "kilogram" -> context.getString(R.string.kilogram)
-            "gram" -> context.getString(R.string.gram)
-            "pound" -> context.getString(R.string.pound)
-            "ounce" -> context.getString(R.string.ounce)
+            // Weight
+            "kilogram (kg)" -> context.getString(R.string.kilogram)
+            "gram (g)" -> context.getString(R.string.gram)
+            "pound (lb)" -> context.getString(R.string.pound)
+            "ounce (oz)" -> context.getString(R.string.ounce)
 
-            // Temperature units
-            "celsius" -> context.getString(R.string.celsius)
-            "fahrenheit" -> context.getString(R.string.fahrenheit)
-            "kelvin" -> context.getString(R.string.kelvin)
+            // Temperature
+            "celsius (°c)" -> context.getString(R.string.celsius)
+            "fahrenheit (°f)" -> context.getString(R.string.fahrenheit)
+            "kelvin (k)" -> context.getString(R.string.kelvin)
 
-            // Time units
-            "second" -> context.getString(R.string.second)
-            "minute" -> context.getString(R.string.minute)
-            "hour" -> context.getString(R.string.hour)
-            "day" -> context.getString(R.string.day)
-            "week" -> context.getString(R.string.week)
+            // Time
+            "second (s)" -> context.getString(R.string.second)
+            "minute (min)" -> context.getString(R.string.minute)
+            "hour (h)" -> context.getString(R.string.hour)
+            "day (d)" -> context.getString(R.string.day)
+            "week (wk)" -> context.getString(R.string.week)
 
-            // Speed units
-            "meters per second"-> context.getString(R.string.meters_per_second)
-            "kilometers per hour" -> context.getString(R.string.kilometers_per_hour)
-            "miles per second" -> context.getString(R.string.miles_per_hour)
-            "feet per hour" -> context.getString(R.string.feet_per_second)
+            // Speed
+            "meters per second (m/s)" -> context.getString(R.string.meters_per_second)
+            "kilometers per hour (km/h)" -> context.getString(R.string.kilometers_per_hour)
+            "miles per hour (mph)" -> context.getString(R.string.miles_per_hour)
+            "feet per second (ft/s)" -> context.getString(R.string.feet_per_second)
 
-            // Energy units
-            "joule" -> context.getString(R.string.joule)
-            "calorie" -> context.getString(R.string.calorie)
-            "kwh" -> context.getString(R.string.kwh)
-            "btu" -> context.getString(R.string.btu)
+            // Energy
+            "joule (j)" -> context.getString(R.string.joule)
+            "calorie (cal)" -> context.getString(R.string.calorie)
+            "kilowatt-hour (kwh)" -> context.getString(R.string.kwh)
+            "british thermal unit (btu)" -> context.getString(R.string.btu)
 
-            // Power units
-            "watt" -> context.getString(R.string.watt)
-            "kilowatt" -> context.getString(R.string.kilowatt)
-            "horsepower" -> context.getString(R.string.horsepower)
+            // Power
+            "watt (w)" -> context.getString(R.string.watt)
+            "kilowatt (kw)" -> context.getString(R.string.kilowatt)
+            "horsepower (hp)" -> context.getString(R.string.horsepower)
 
-            // Torque units
-            "newton meter" -> context.getString(R.string.newton_meter)
-            "kilogram meter" -> context.getString(R.string.kilogram_meter)
-            "pound foot" -> context.getString(R.string.pound_foot)
+            // Torque
+            "newton meter (n·m)" -> context.getString(R.string.newton_meter)
+            "kilogram meter (kg·m)" -> context.getString(R.string.kilogram_meter)
+            "pound foot (lb·ft)" -> context.getString(R.string.pound_foot)
 
-            // Pressure units
-            "pascal" -> context.getString(R.string.pascal)
-            "kilopascal" -> context.getString(R.string.kilopascal)
-            "bar" -> context.getString(R.string.bar)
-            "psi" -> context.getString(R.string.psi)
+            // Pressure
+            "pascal (pa)" -> context.getString(R.string.pascal)
+            "kilopascal (kpa)" -> context.getString(R.string.kilopascal)
+            "bar (bar)" -> context.getString(R.string.bar)
+            "pounds per square inch (psi)" -> context.getString(R.string.psi)
 
-            // Force units
-            "newton" -> context.getString(R.string.newton)
-            "kilogram_force" -> context.getString(R.string.kilogram_force)
-            "pound_force" -> context.getString(R.string.pound_force)
+            // Force
+            "newton (n)" -> context.getString(R.string.newton)
+            "kilogram-force (kgf)" -> context.getString(R.string.kilogram_force)
+            "pound-force (lbf)" -> context.getString(R.string.pound_force)
 
-            // Angle units
-            "degree" -> context.getString(R.string.degree)
-            "radian" -> context.getString(R.string.radian)
-            "gradian" -> context.getString(R.string.gradian)
+            // Angle
+            "degree (°)" -> context.getString(R.string.degree)
+            "radian (rad)" -> context.getString(R.string.radian)
+            "gradian (gon)" -> context.getString(R.string.gradian)
 
             // Currency
-            "usd" -> context.getString(R.string.usd)
-            "eur" -> context.getString(R.string.eur)
-            "gbp" -> context.getString(R.string.gbp)
-            "jpy" -> context.getString(R.string.jpy)
-            "pkr" -> context.getString(R.string.pkr)
+            "us dollar (usd)" -> context.getString(R.string.usd)
+            "euro (eur)" -> context.getString(R.string.eur)
+            "british pound (gbp)" -> context.getString(R.string.gbp)
+            "japanese yen (jpy)" -> context.getString(R.string.jpy)
+            "pakistani rupee (pkr)" -> context.getString(R.string.pkr)
 
             // Sound
-            "hertz" -> context.getString(R.string.hertz)
-            "kilohertz" -> context.getString(R.string.kilohertz)
-            "megahertz" -> context.getString(R.string.megahertz)
-            "pascal_unit" -> context.getString(R.string.pascal_unit)
-            "micropascal" -> context.getString(R.string.micropascal)
-            "decibel" -> context.getString(R.string.decibel)
-            "watt_per_square_meter" -> context.getString(R.string.watt_per_square_meter)
+            "hertz (hz)" -> context.getString(R.string.hertz)
+            "kilohertz (khz)" -> context.getString(R.string.kilohertz)
+            "megahertz (mhz)" -> context.getString(R.string.megahertz)
+            "micropascal (µpa)" -> context.getString(R.string.micropascal)
+            "decibel (db)" -> context.getString(R.string.decibel)
+            "watt per square meter (w/m²)" -> context.getString(R.string.watt_per_square_meter)
 
             // BMI
-            "metric" -> context.getString(R.string.metric)
-            "imperial" -> context.getString(R.string.imperial)
+            "metric (kg/m²)" -> context.getString(R.string.metric)
+            "imperial (lb/in²)" -> context.getString(R.string.imperial)
 
             else -> key
         }
     }
+
 
 
 
